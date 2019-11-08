194500d2aa9a65484ba34cc0b702a7b8ad6cedeb
patrickRobotics <pirungu9@gmail.com>
2019-11-08 17:35:51 +0300

    Fix test runner task

--- src/tests/server.test.js
+++ src/tests/server.test.js
@@ -6,15 +6,17 @@ const server = require('../index');
 describe('Test App Server', () => {
     const url = 'http://localhost:3000/';
     // eslint-disable-next-line no-undef
-    it('should return statuscode 200', () => {
-        request(url, (error, response, body) => {
+    it('should return statuscode 200', (done) => {
+        request(url, (error, response) => {
             expect(response.statusCode).to.equal(200);
+            done();
         });
     });
     // eslint-disable-next-line no-undef
-    it('should return body json text', () => {
+    it('should return body json text', (done) => {
         request(url, (error, response, body) => {
-            expect(response.body).to.equal('{"info":"Node.js, Express, and Postgres API"}');
+            expect(body).to.equal('{"info":"Node.js, Express, and Postgres API"}');
+            done();
         });
     });
 });
