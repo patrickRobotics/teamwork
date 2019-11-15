1a234f377b952eeb4b476352323ae62174a65d2a
patrickRobotics <pirungu9@gmail.com>
2019-11-15 14:17:24 +0300

    Complete posts APIs

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -23,14 +23,42 @@ exports.getPosts = (req, res) => {
     });
 };
 
+exports.getPostById = (req, res) => {
+    // eslint-disable-next-line radix
+    const postId = parseInt(req.params.id);
+    pool.connect((err, client, done) => {
+        const query = 'SELECT id, articletitle, article, authorid, flagged, createdon FROM posts WHERE id = $1';
+        client.query(query, [postId], (error, result) => {
+            done();
+            if (error) {
+                res.status(400).json({
+                    status: 'error',
+                    error: 'An error occurred with your query',
+                });
+            }
+            if (result.rows < '1') {
+                res.status(404).send({
+                    status: 'error',
+                    error: 'User with that id was not found',
+                });
+            } else {
+                res.status(200).send({
+                    status: 'success',
+                    data: result.rows,
+                });
+            }
+        });
+    });
+};
+
 exports.createPost = (req, res) => {
     const data = {
         articleTitle: req.body.articleTitle,
         article: req.body.article,
-        authorId: 3,
+        authorId: req.body.authorId,
     };
     pool.connect((err, client, done) => {
-        const query = 'INSERT INTO posts(articleTitle, article, authorId) VALUES($1,$2,$3) RETURNING *';
+        const query = 'INSERT INTO posts(articletitle, article, authorid) VALUES($1,$2,$3) RETURNING *';
         const values = [data.articleTitle, data.article, data.authorId];
         client.query(query, values, (error, result) => {
             done();
@@ -43,6 +71,56 @@ exports.createPost = (req, res) => {
                 res.status(202).send({
                     status: 'success',
                     result: result.rows[0],
+                });
+            }
+        });
+    });
+};
+
+exports.updatePost = (req, res) => {
+    // eslint-disable-next-line radix
+    const postId = parseInt(req.params.id);
+    const data = {
+        articleTitle: req.body.articleTitle,
+        article: req.body.article,
+        authorId: req.body.authorId,
+    };
+    pool.connect((err, client, done) => {
+        client.query(
+            'UPDATE posts SET articletitle=$2, article=$3, authorid=$4 WHERE id = $1',
+            [postId, data.articleTitle, data.article, data.authorId],
+            (error) => {
+                done();
+                if (error) {
+                    res.status(400).json({
+                        status: 'error',
+                        error: 'An error occurred with your query',
+                    });
+                } else {
+                    res.status(202).send({
+                        status: 'success',
+                    });
+                }
+            },
+        );
+    });
+};
+
+exports.deletePost = (req, res) => {
+    // eslint-disable-next-line radix
+    const postId = parseInt(req.params.id);
+    pool.connect((er, client, done) => {
+        const query = 'DELETE from posts WHERE id = $1';
+        client.query(query, [postId], (error) => {
+            done();
+            if (error) {
+                res.status(400).json({
+                    status: 'error',
+                    error: 'An error occurred with your query',
+                });
+            } else {
+                res.status(200).send({
+                    status: 'success',
                 });
             }
         });
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -7,7 +7,11 @@ const auth = require('../middleware/auth');
 const routesCtrl = require('../controllers/posts');
 
 router.post('/posts', auth.isAuthorized, routesCtrl.createPost);
+router.put('/posts/:id', auth.isAuthorized, routesCtrl.updatePost);
 
 router.get('/posts', auth.isAuthorized, routesCtrl.getPosts);
+router.get('/posts/:id', auth.isAuthorized, routesCtrl.getPostById);
+
+router.delete('/posts/:id', auth.isAuthorized, routesCtrl.deletePost);
 
 module.exports = router;
