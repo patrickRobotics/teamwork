6bbbcad732ea0fa04ef5fb1522cae62713d0b57a
patrickRobotics <pirungu9@gmail.com>
2019-11-19 01:31:51 +0300

    Add Swagger UI for documenting app APIs

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 const cors = require('cors');
 
 const userRoutes = require('./routes/users');
 const postsRoutes = require('./routes/posts');
 const gifRoutes = require('./routes/gifs');
 const feedRoutes = require('./routes/feeds');
+
+const swaggerDocument = require('./swagger/swagger.json');
 
 const app = express();
 
@@ -26,5 +29,7 @@ app.use('/api/v1/auth', userRoutes);
 app.use('/api/v1/', postsRoutes);
 app.use('/api/v1/', gifRoutes);
 app.use('/api/v1/', feedRoutes);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
