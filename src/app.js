58df188205c5f8742c3f41253fb9a80a4948d726
patrickRobotics <pirungu9@gmail.com>
2019-11-19 23:39:19 +0300

    Start Swagger documentation for Users apis

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const swaggerUi = require('swagger-ui-express');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerSpec = require('./services/swaggerConfig');
 
 const userRoutes = require('./routes/users');
 const postsRoutes = require('./routes/posts');
 const gifRoutes = require('./routes/gifs');
 const feedRoutes = require('./routes/feeds');
-
-const swaggerDocument = require('./swagger/swagger.json');
 
 const app = express();
 
@@ -30,6 +30,6 @@ app.use('/api/v1/', postsRoutes);
 app.use('/api/v1/', gifRoutes);
 app.use('/api/v1/', feedRoutes);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 module.exports = app;
---
+++ src/services/swaggerConfig.js
@@ -0,0 +1,26 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const swaggerDefinition = {
+    info: {
+        title: 'Teamwork APIs documentation',
+        version: '1.0.0',
+        description: 'Endpoints to test the DevC Training with Andela Capstone Project routes',
+    },
+    host: 'localhost:3000',
+    basePath: '/api/v1/',
+    securityDefinitions: {
+        Bearer: {
+            type: 'apiKey',
+            name: 'Authorization',
+            scheme: 'bearer',
+            in: 'header'
+        },
+    }
+};
+
+const options = {
+    swaggerDefinition,
+    apis: ['**/*.yaml'],
+};
+
+module.exports = swaggerJSDoc(options);
