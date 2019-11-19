02ceb83a447b2d09af2d474d8757c32ef1adef39
patrickRobotics <pirungu9@gmail.com>
2019-11-19 23:58:12 +0300

    refactor api routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,12 +7,12 @@ const admin = require('../middleware/adminCheck');
 
 const employeeCtrl = require('../controllers/employees');
 
-router.post('/login', employeeCtrl.login);
+router.post('/signin', employeeCtrl.login);
 
 router.get('/users', auth.isAuthorized, employeeCtrl.getUsers);
 router.get('/users/:id', auth.isAuthorized, employeeCtrl.getUserById);
 
-router.post('/users', auth.isAuthorized, admin.isAdmin, employeeCtrl.createUser);
+router.post('/create-user', auth.isAuthorized, admin.isAdmin, employeeCtrl.createUser);
 router.put('/users/:id', auth.isAuthorized, admin.isAdmin, employeeCtrl.updateUser);
 router.delete('/users/:id', auth.isAuthorized, admin.isAdmin, employeeCtrl.deleteUser);
 
